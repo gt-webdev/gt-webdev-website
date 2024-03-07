698d5d81931a72facd5cc754152992ca501a5bee
Stephen Buckley <sbuckley@gatech.edu>
2024-03-07 17:34:04 -0500

    Add firebase connection demo

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,16 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import NavBar from "../components/NavBar";
 import BottomBar from "../components/BottomBar";
 import Head from 'next/head'
 import {Grid, Typography, Box} from "@mui/material";
 
 import {currentData,previousData} from '../data/projectData.js';
+
+// Import firebase database
+import firebaseApp from "../helpers/firebase.js";
+import { getFirestore, doc, collection, getDocs } from "firebase/firestore";
+
+const db = getFirestore(firebaseApp);
 
 const Projects = () => {
 
@@ -15,12 +21,29 @@ const Projects = () => {
         height: '15rem',
       };
 
+    const [projects, setProjects] = useState([]);
+
+    // When the application loads, get all projects from the database
+    useEffect(async () => {
+        const projectSnapshot = await getDocs(collection(db, "projects"));
+        const projects = [];
+        projectSnapshot.forEach(doc => {
+            projects.push(doc.data());
+        })
+        setProjects(projects);
+    }, []);
+
     return(
         <div>
             <Head>
                 <title>Projects | GT WebDev</title>
             </Head>
             <NavBar />
+
+            <Typography variant="p">Showing Projects from Database:</Typography>
+            {projects.map(project => {
+                return <p>{project['name']}</p>
+            })}
 
             {/* Current Semster Project List */}
             <Typography variant="h3" className="projectSectionTitle">
