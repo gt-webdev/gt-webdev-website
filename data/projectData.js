f7818bd0d7474dc4ff09b8e8fae7c92dc95cbda2
Joshua <jkim3590@gatech.edu>
2024-04-19 13:01:22 -0400

    Updated projectData to include past and current semester projects. Implemented onClick action to the cards to reveal project link. Tried to implement card flipping, but need to resolve

--- data/projectData.js
+++ data/projectData.js
@@ -1,60 +1,111 @@
 let currentData = [
     {
-        "semester": "Spring 2022",
+        "semester": "Spring 2024",
         "projects": [
             {
-                "name": "YouTube to Spotify",
-                "description": "A tool that converts Youtube playlists to Spotify playlists",
-                "manager": "Vignesh Sreedhar",
-                "members": [],
+                "name": "Punchshot Pickleball",
+                "description": "The new one-stop-shop for pickleball players to join and create teams and compete in leagues",
+                "manager": "Vignesh Sreedhar & Logan", 
+                "members": [], 
+                "link": ""
+            }, 
+            {
+                "name": "Automated Financial App",
+                "description": "A web app that automatically takes data from your bank using Plaid API and automatically budgets for you",
+                "manager": "Oscar Zhang", 
+                "members": [], 
+                "link": ""
+            }, 
+            {
+                "name": "Collaborative Whiteboard App",
+                "description": "Create, collaborate, and share notes and ideas with this online whiteboard",
+                "manager": "Timothy Sung", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "COVID-19 Data Visualizer",
-                "description": "A web application to visualize COVID-19 data",
-                "manager": "Stephen Buckley",
-                "members": [],
+                "name": "Event Ticketing and Management Platform",
+                "description": "Similar to Corq or Eventbrite, you create and manage events, manage and sell tickets for created events, find nearby events and purchase tickets, and generate QR code tickets",
+                "manager": "Ian Wood", 
+                "members": [], 
+                "link": ""
+            }, 
+            {
+                "name": "Streamify",
+                "description": "A web app, a social audio platform that allows users to create and join live audio rooms for conversations, discussions, and interactive experiences",
+                "manager": "Tuan", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Video Chat",
-                "description": "A web application version of Zoom",
-                "manager": "Deepak Ramalingam",
-                "members": [],
+                "name": "GTStudyHive",
+                "description": "Application allowing users to drop pins on Georgia Tech’s campus to start study sessions & join existing sessions.  Pins specify duration, specific location (ie. floor, other identifiers), current capacity, if it is class-specific or general, and more. Pins can be dropped by users at places like Crossland, Tech Green, NAV, Student Center, buildings (Klaus, CoC), etc.",
+                "manager": "Ananya", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Video Chat",
-                "description": "A web application version of Zoom",
-                "manager": "Deepak Ramalingam",
-                "members": [],
+                "name": "Video Streaming Platform",
+                "description": "Similar to Youtube, this is a website that allows users to upload and view videos.",
+                "manager": "Daiwik Pal", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Classic Board Game",
-                "description": "Implements a classic board game in the browser like tictactoe, checkers, and chess",
-                "manager": "Talia Tian",
-                "members": [],
+                "name": "Degree Planner",
+                "description": "A fun interactive website to plan semesters",
+                "manager": "Noah Nguyen", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Essay Tools Dashboard",
-                "description": "A tool to help users to better write essays",
-                "manager": "Jakob Bjorner",
-                "members": [],
+                "name": "Dungeons & Dragons",
+                "description": "A web application where players can join their friend’s group and try to win their way out of a dungeon created by ChatGPT. A UI will allow players to choose actions for their characters",
+                "manager": "Laksh Makhija", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Essay Tools Dashboard",
-                "description": "A tool to help users to better write essays",
-                "manager": "Jakob Bjorner",
-                "members": [],
+                "name": "Skru",
+                "description": "A paletted bitmap editor for the web",
+                "manager": "Race Williams & Naquan Smith", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Protest Website",
-                "description": "A website for students to create and find protests anonymously",
-                "manager": "Jinwoo Park",
-                "members": [],
+                "name": "r/GTPlace",
+                "description": "GT’s very own pixel art mural web app",
+                "manager": "Allison Vu", 
+                "members": [], 
+                "link": ""
             },
             {
-                "name": "Valorant Sharing Site",
-                "description": "Website to share gaming related media",
-                "manager": "Jacob Amin",
-                "members": [],
+                "name": " AI Conversation Coach App",
+                "description": "A website that simulates a conversation or provides response ideas",
+                "manager": "Naquan Smith", 
+                "members": [], 
+                "link": ""
+            },
+            {
+                "name": "GT Marketplace",
+                "description": "Verified platform for college students and faculty to buy/sell, rent/sublease apartments, find tutors/carpools/etc.", 
+                "manager": "Shreyas Goyal & Aditya Kabu", 
+                "members": [], 
+                "link": ""
+            },
+            {
+                "name": "FotoMind",
+                "description": ": Smart Photo Management System that will automatically organize images based on criteria. Will have a chatbot feature to help users navigate the page and help answer any questions related to the platform.",
+                "manager": "Varun Vudathu", 
+                "members": [], 
+                "link": ""
+            },
+            {
+                "name": "HouseMe",
+                "description": "Application that will connect interns and co-ops who need to relocate for short periods of time with others as well as apartment owners",
+                "manager": "Josh & Amy", 
+                "members": [], 
+                "link": ""
             },
         ]
     }
@@ -62,55 +113,192 @@ let currentData = [
 
 let previousData = [
     {
+        "semester": "Fall 2023",
+        "projects": [
+            {
+                "name": "Fashion GPT",
+                "description": "Use AI to generate outfits for users according to their preferences",
+                "manager": "Deepak",
+                "members": [],
+                "link": "https://github.com/orgs/FashionGPT/repositories"
+            },
+            {
+                "name": "Punchshot Pickelball",
+                "description": "Web Application that allows users to find local tournaments, join practice leagues, and learn more about pickleball.",
+                "manager": "Vignesh & Logan",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "BizWeb",
+                "description": "A web application enabling project selection for clients, such as generating transaction receipts and connect with printer, offering discounts, creating daily/yearly sales reports, maintaining a notifications table, managing client profiles, etc. The main purpose is to monitor employee interactions with customer transactions, thereby saving time and reducing costs associated with tax filing due to transaction recording.",
+                "manager": "Nina",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Travel App",
+                "description": "An app that allows you to log where you’ve traveled and been to, visualize how much of the world you explored, and adding locations to a wishlist",
+                "manager": "Michelle",
+                "members": [],
+                "link": "https://github.com/example/client-repo"
+            },
+            {
+                "name": "Social Cookbook",
+                "description": "Instagram-like clone focused on sharing recipes",
+                "manager": "Ian",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Group Planning",
+                "description": "A web app that makes it easier for people to share and plan events",
+                "manager": "Zini & Amy",
+                "members": [],
+                "link": "https://github.com/GT-WebDev-Group-Planning/group-planning"
+            },
+            {
+                "name": "GT Club Explorer",
+                "description": "A user-friendly website that allows GT students browse clubs more easily and faster",
+                "manager": "Ganning",
+                "members": [],
+                "link": "https://github.com/ganning127/ClubExplorer "
+            },
+            {
+                "name": "Restaurant Review Manager",
+                "description": "App that allows restaurants to efficiently manage customer reviews of their restaurants",
+                "manager": "Hoang & Stuti",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "GT Reviews",
+                "description": "Platform for GT students to post and read reviews on dorms, apartments, dining halls, professors, classes, or anything else GT related",
+                "manager": "Josh & Nick",
+                "members": [],
+                "link": "https://github.com/buzzwalk/reviews"
+            },
+            {
+                "name": "GT WebDev Project Platform",
+                "description": "Platform to empower GT WebDev members to showcase their projects; Foster collaboration among members, highlight GT WebDev’s achievements",
+                "manager": "Nimita",
+                "members": [],
+                "link": ""
+            }
+
+        ]
+    },
+    {
+        "semester": "Spring 2022",
+        "projects": [
+            {
+                "name": "YouTube to Spotify",
+                "description": "A tool that converts Youtube playlists to Spotify playlists",
+                "manager": "Vignesh Sreedhar",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "COVID-19 Data Visualizer",
+                "description": "A web application to visualize COVID-19 data",
+                "manager": "Stephen Buckley",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Video Chat",
+                "description": "A web application version of Zoom",
+                "manager": "Deepak Ramalingam",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Classic Board Game",
+                "description": "Implements a classic board game in the browser like tictactoe, checkers, and chess",
+                "manager": "Talia Tian",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Essay Tools Dashboard",
+                "description": "A tool to help users to better write essays",
+                "manager": "Jakob Bjorner",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Protest Website",
+                "description": "A website for students to create and find protests anonymously",
+                "manager": "Jinwoo Park",
+                "members": [],
+                "link": ""
+            },
+            {
+                "name": "Valorant Sharing Site",
+                "description": "Website to share gaming related media",
+                "manager": "Jacob Amin",
+                "members": [],
+                "link": ""
+            },
+        ]
+    },
+    {
         "semester": "Fall 2021",
         "projects": [
             {
                 "name": "Spotify Analytics Tool",
                 "description": "A tool similar to Spotify Wrapped used to analyze and show trends in people's Spotify listening data",
                 "manager": "Vignesh Sreedhar",
-                "members": []
+                "members": [],
             },
             {
                 "name": "Data Converter",
                 "description": "A web application to convert between different files types, such as between PNG and PDF",
                 "manager": "Stephen Buckley",
-                "members": []
+                "members": [],
+                "link": ""
             },
             {
                 "name": "Real-Time Chat",
                 "description": "A website that fosters 1-1 and group conversation with real-time updates",
                 "manager": "Deepak Ramalingam",
-                "members": []
+                "members": [],
+                "link": ""
             },
             {
                 "name": "Browser Game",
                 "description": "An in-browser game in which a player moves around and avoids obstacles",
                 "manager": "Deepak Ramalingam",
-                "members": []
+                "members": [],
+                "link": ""
             },
             {
                 "name": "Protest Website",
                 "description": "A social-media-like portal in which people can plan and join protests",
                 "manager": "Jin Park",
-                "members": []
+                "members": [],
+                "link": ""
             },
             {
                 "name": "Essay Dashboard",
                 "description": "A web application that shows examples of essays and related academic content",
                 "manager": "Pranay Shah",
-                "members": []
+                "members": [],
+                "link": ""
             },
             {
                 "name": "Cryptocurrency Portfolio Dashboard",
                 "description": "A web application to visualize and track cryptocurrency holdings",
                 "manager": "Talia Tian",
-                "members": []
+                "members": [],
+                "link": ""
             },
             {
                 "name": "Cryptocurrency Exchange",
                 "description": "A tool to make exchanges between different forms of cryptocurrency",
                 "manager": "Bek Hovakimian",
-                "members": []
+                "members": [],
+                "link": ""
             }
         ]
     },
@@ -121,13 +309,15 @@ let previousData = [
                 "name": "GT WebDev Website",
                 "description": "This very website! Most of the architecture and planning was done",
                 "manager": "Stephen Buckley",
-                "members": ["Stephen Buckley", "Zhao Chen"]
+                "members": ["Stephen Buckley", "Zhao Chen"],
+                "link": ""
             },
             {
                 "name": "Internship Tracker",
                 "description": "A tool to track a student's internship applications and their statuses",
                 "manager": "Dane Koval",
-                "members": ["Dane Koval", "Jason Jiang"]
+                "members": ["Dane Koval", "Jason Jiang"],
+                "link": ""
             }
         ]
     }
--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 import BottomBar from "../components/BottomBar";
 import Head from 'next/head'
+import ReactCardFlip from 'react-card-flip';
 import {Grid, Typography, Box} from "@mui/material";
 
 import {currentData,previousData} from '../data/projectData.js';
@@ -15,6 +17,71 @@ const Projects = () => {
         height: '15rem',
       };
 
+    /* 
+    Flippable card as a box.
+    Card with project title. Mouse hover to flip card and reveal project description
+    Bugs:
+    - Project description on the same side as title and only momentarily reveals when you first hover
+    - Card flips multiple times when hovering over
+    - Project title not in the middle of the card
+    
+    To implement: 
+    - Click on anywhere on card except title to flip reveal description.
+    - Click on title to open link to project/github
+    */ 
+
+    const FlippableBox = ({ project }) => {
+        const [isFlipped, setIsFlipped] = useState(false);
+        const handleTextClick = () => {
+            if (project["link"]) {
+                window.open(project["link"], '_blank');
+            }
+        };
+        const handleBoxClick = () => {
+            if (project["link"]) {
+                window.open(project["link"], '_blank');
+            }
+        };
+        // const handleBoxClick = () => {
+        //     setIsFlipped(!isFlipped);
+        // };
+        const handleBoxHover = () => {
+            setIsFlipped(true);
+        };
+        const handleBoxLeave = () => {
+            setIsFlipped(false);
+        }
+
+        return (
+            <Box
+                sx={{
+                    ...commonStyles,
+                    borderRadius: 5,
+                    cursor: 'pointer',
+                    transformStyle: 'preserve-3d',
+                    transition: 'transform 0.5s',
+                    transform: isFlipped ? 'rotateY(180deg)' : 'none'
+                }}
+                className="projectBoxInfo projectBoxTxt"
+                onMouseEnter={handleBoxHover}
+                onMouseLeave={handleBoxLeave}
+                onClick={handleBoxClick}
+            >
+                <Box
+                    sx={{
+                        ...commonStyles,
+                        backfaceVisibility: 'hidden'
+                    }}
+                >
+                    <Typography>{project['name']}</Typography>
+                    {isFlipped && (
+                        <Typography>{project['description']}</Typography>
+                    )}
+                </Box>
+            </Box>
+        );
+    };
+
     return(
         <div>
             <Head>
@@ -23,20 +90,21 @@ const Projects = () => {
             <NavBar />
 
             {/* Current Semster Project List */}
+            {/* Testing flippableBox on current projects only*/}
             <Typography variant="h3" className="projectSectionTitle">
                 {currentData[0]['semester']}
             </Typography>
             <Grid container columnGap={2} className="projectSectionPadding">
-                {currentData[0]['projects'].map((cur, ind) => {
-                    return(
-                        <Box sx={{ ...commonStyles, borderRadius: 5 }} className="projectBoxInfo projectBoxTxt">
-                            {cur['name']}
-                        </Box>
-                    )
-                })}
+                {currentData[0]['projects'].map((cur, ind) => ( 
+                    <FlippableBox key={ind} project={cur} />
+                ))}
             </Grid>
-
+ 
             {/* List of previous semester projects */}
+            {/* Instead of flippable box, click the card to open the project link.
+                - To do:
+                    - Implement project description reveal
+                    - Implement Flippable Card from current semester*/}
             {previousData.map((cur, ind) => {
                 return (
                     <div>
@@ -45,8 +113,14 @@ const Projects = () => {
                         </Typography>
                         <Grid container columnGap={2} className="projectSectionPadding">
                             {cur['projects'].map((cur, ind) => {
+                                const onClickHandler = cur["link"] ? 
+                                    () => window.open(cur["link"], '_blank') : 
+                                    () => {};
                                 return(
-                                    <Box sx={{ ...commonStyles, borderRadius: 5 }} className="projectBoxInfo projectBoxTxt">
+                                    <Box sx={{ ...commonStyles, borderRadius: 5, cursor: 'pointer'}} 
+                                             className="projectBoxInfo projectBoxTxt"
+                                             onClick={onClickHandler}
+                                    >
                                         {cur['name']}
                                     </Box>
                                 )
